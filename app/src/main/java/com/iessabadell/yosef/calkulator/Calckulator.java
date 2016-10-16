b315c86578d797c635f16bb4e7e8db31bfec25d7
yosef <insyosef@gmail.com>
2016-10-16 18:32:41 +0200

    Project creation

---
+++ app/src/main/java/com/iessabadell/yosef/calkulator/Calckulator.java
@@ -0,0 +1,13 @@
+package com.iessabadell.yosef.calkulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calckulator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calckulator);
+    }
+}
